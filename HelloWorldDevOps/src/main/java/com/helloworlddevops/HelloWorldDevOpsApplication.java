b6e719141b713668b8f49e584b80ad20b22ff342
MilosMrdja <mrdja.sv34.2022@uns.ac.rs>
2026-04-18 14:56:41 +0200

    [feat] add Maven Web App, Dockerfile & Docker-compose

---
+++ HelloWorldDevOps/src/main/java/com/helloworlddevops/HelloWorldDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.helloworlddevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldDevOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldDevOpsApplication.class, args);
+    }
+
+}
